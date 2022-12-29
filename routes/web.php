27a00664bd8d094ec587988fdc24f3d7c1ff34ea
feggy <feggy@limecommerce.com>
2022-12-29 23:27:55 +0700

    view

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,12 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('products', [ProductController::class, 'index'])->name('products.index');
-Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('products', [ProductController::class, 'store'])->name('products.store');
-Route::get('products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::put('products/{id}', [ProductController::class, 'update']);
-Route::delete('products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/articles', [ArticleController::class, 'index']);
