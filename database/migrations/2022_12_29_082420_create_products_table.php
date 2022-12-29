39e5f324303746962c0d714d426c15a2f833fae5
feggy <feggy@limecommerce.com>
2022-12-29 23:27:49 +0700

    migration

--- database/migrations/2022_12_29_082420_create_products_table.php
+++ database/migrations/2022_12_29_082420_create_products_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', '100')->nullable(false);
+            $table->double('price')->nullable(false);
+            $table->bigInteger('stock')->default(0);
             $table->timestamps();
         });
     }
