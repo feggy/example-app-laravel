70d45cb19b8aae625c5dfd974210f0e7872dad3b
feggy <feggy@limecommerce.com>
2022-12-29 23:27:39 +0700

    Seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::insert(
+            [
+                'name' => 'Feggy',
+                'price' => 10000,
+                'stock' => 10,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
+        );
+    }
+}
